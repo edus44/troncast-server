dcc869438e68de09f5ebe4d05db81d0078ee66ec
edus44 <eduhhg@gmail.com>
2017-01-29 12:13:31 +0100

    socket message router

--- socket.js
+++ socket.js
@@ -2,62 +2,34 @@
 
 const debug = require('debug')('tc:server:socket')
 
-// const forwarder = require('./lib/forwarder');
-// const channelsManager = require('./lib/channelsManager');
+const forwarder = require('./lib/forwarder')
 
-let localIp
-require('dns').lookup(require('os').hostname(), (err,addr) => {
-    localIp = addr
-})
+// let localIp
+// require('dns').lookup(require('os').hostname(), (err,addr) => {
+//     localIp = addr
+// })
 
 module.exports = function(io){
 
-    io.on('connection',(socket)=>{
-        debug('connected',socket.id)
-    })
-
     debug('init')
-    /*eslint-disable*/
-    return;
 
     const ns = {
-        channel : io.of('/channel'),
+        repository : io.of('/repository'),
         remote : io.of('/remote'),
         screen : io.of('/screen')
     }
 
-    forwarder(ns);
+    forwarder(ns)
 
-    ///////////
-    //SCREEN //
-    ///////////
     ns.screen.on('connection',(socket)=>{
         debug('screen connected',socket.id)
-
-        socket.on('getRemoteUri',function(cb){
-
-            cb('http://'+localIp+':13370')
-        })
     })
 
-    ///////////
-    //REMOTE //
-    ///////////
     ns.remote.on('connection',(socket)=>{
         debug('remote connected',socket.id)
-
-
-        socket.on('getChannels',function(cb){
-            cb(channelsManager.getList())
-        })
-
-        socket.on('robotCommand',robot.command)
     })
 
-    ////////////
-    //CHANNEL //
-    ////////////
-    ns.channel.on('connection',(socket)=>{
-        debug('channel connected',socket.id)
+    ns.repository.on('connection',(socket)=>{
+        debug('repository connected',socket.id)
     })
 }
