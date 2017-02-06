f405d4d7807460dabca6ac6f1e0f3a08ed934bad
edus44 <eduhhg@gmail.com>
2017-02-07 00:38:05 +0100

    added some example channels

---
+++ channels/browser/preload.js
@@ -0,0 +1,34 @@
+/* global TroncastChannel, window */
+/* eslint-disable no-constant-condition */
+'use strict'
+
+TroncastChannel.onConfigure(function*(){
+    let result
+    while(true){
+        result = yield {
+            title:'Load url',
+            elements:[
+                {name:'url'         , label:'Url'          , type:'input'}  ,
+                {name:'navigate'    , label:'Navigate'     , type:'button'} ,
+                {name:'scrollUp'    , label:'Scroll up'    , type:'button'} ,
+                {name:'scrollDown'  , label:'Scroll down'  , type:'button'} ,
+                {name:'scrollLeft'  , label:'Scroll left'  , type:'button'} ,
+                {name:'scrollRight' , label:'Scroll right' , type:'button'} ,
+            ]
+        }
+        if (result.navigate)
+            window.location.href = result.url
+        else
+        if (result.scrollDown)
+            window.scrollTo( window.scrollX, window.scrollY+100)
+        else
+        if (result.scrollUp)
+            window.scrollTo( window.scrollX, window.scrollY-100)
+        else
+        if (result.scrollLeft)
+            window.scrollTo( window.scrollX-100, window.scrollY)
+        else
+        if (result.scrollRight)
+            window.scrollTo( window.scrollX+100, window.scrollY)
+    }
+})
